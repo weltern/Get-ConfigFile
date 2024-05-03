bfa076d9ca99bf6c0ed1ffca23fb9abc71170fed
Nick Welter <37944729+weltern@users.noreply.github.com>
2024-05-02 23:14:39 -0400

    Update Get-ConfigFile.ps1
    Added code to help against http requests for files on server with no cert.

--- Get-ConfigFile.ps1
+++ Get-ConfigFile.ps1
@@ -1,4 +1,5 @@
-function Get-ConfigFile {
+function Get-ConfigFile
+{
     <#
     .SYNOPSIS
         Downloads/Copies over a config file from a remote location
@@ -20,75 +21,129 @@ function Get-ConfigFile {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [String]$DownloadSource,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [String]$Destination
     )
-    process {
+    process
+    {
         #Determine if download source is a URL or UNC path
         Write-Verbose -Message "Checking download source type"
-        if($DownloadSource -like "\\*"){
+        if ($DownloadSource -like "\\*")
+        {
             $DownloadSourceType = "UNC"
             Write-Verbose -Message "Download source type: UNC"
-        }elseif ($DownloadSource -like "http*") {
+        }
+        elseif ($DownloadSource -like "http*")
+        {
             $DownloadSourceType = "HTTP"
             Write-Verbose -Message "Download source type: HTTP"
-        }else{
+        }
+        else
+        {
             Write-Error -Message "Unable to determine download source type. Please verify DownloadSource parameter."
             throw
         }
 
         #Test paths of Locations
         Write-Verbose -Message "Testing Path of Downloadsource"
-        if($DownloadSourceType -eq "UNC"){
+        if ($DownloadSourceType -eq "UNC")
+        {
             #UNC Test-Path
-            if(-not(Test-Path -Path $DownloadSource -ErrorAction SilentlyContinue)){
+            if (-not(Test-Path -Path $DownloadSource -ErrorAction SilentlyContinue))
+            {
                 Write-Error -Message "Was unable to reach the DownloadSource: $DownloadSource. Please verify DownloadSource parameter."
                 throw
-            }else{
+            }
+            else
+            {
                 Write-Verbose -Message "Verified DownloadSource location"
             }
-        }else{
+        }
+        else
+        {
             #HTTP Test Path
-            if(-not((Invoke-WebRequest -Uri $DownloadSource).Statuscode -eq 200)){
+            $certCallback = @"
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public class ServerCertificateValidationCallback
+{
+    public static void Ignore()
+    {
+        if(ServicePointManager.ServerCertificateValidationCallback ==null)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += 
+                delegate
+                (
+                    Object obj, 
+                    X509Certificate certificate, 
+                    X509Chain chain, 
+                    SslPolicyErrors errors
+                )
+                {
+                    return true;
+                };
+        }
+    }
+}
+"@
+            Add-Type $certCallback
+            [ServerCertificateValidationCallback]::Ignore()
+            if (-not((Invoke-WebRequest -Uri $DownloadSource -UseBasicParsing).Statuscode -eq 200))
+            {
                 Write-Error -Message "Was unable to reach the DownloadSource: $DownloadSource. Please verify DownloadSource parameter."
                 throw
-            }else{
+            }
+            else
+            {
                 Write-Verbose -Message "Verified DownloadSource location"
             }
         }
 
         #Download the config file
         Write-Verbose -Message "Attemping to download the config file"
-        if($DownloadSourceType -eq "UNC"){
+        if ($DownloadSourceType -eq "UNC")
+        {
             #UNC Copy
-            try {
+            try
+            {
                 Copy-Item -Path $DownloadSource -Destination $Destination -Force -ErrorAction Stop
                 $ConfigFileLocation = $Destination
                 Write-Verbose -Message "ConfigFile successfully downloaded"
             }
-            catch {
+            catch
+            {
                 Write-Error -Message "Download of Config file failed."
                 throw
             }
-        }else{
+        }
+        else
+        {
             #HTTP download
-            try {
-                Invoke-WebRequest -Uri $DownloadSource -OutFile $Destination -Force -ErrorAction Stop
+            try
+            {
+                Invoke-WebRequest -Uri $DownloadSource -OutFile $Destination -UseBasicParsing -ErrorAction Stop
                 $ConfigFileLocation = $Destination
                 Write-Verbose -Message "ConfigFile successfully downloaded"
             }
-            catch {
+            catch
+            {
                 Write-Error -Message "Download of Config file failed."
                 throw
             }
         }
     }
-    end {
-        if(Test-Path -Path $ConfigFileLocation -ErrorAction SilentlyContinue){
+    end
+    {
+        if (Test-Path -Path $ConfigFileLocation -ErrorAction SilentlyContinue)
+        {
             return $ConfigFileLocation
-        }else {
+        }
+        else
+        {
             Write-Error -Message "Was unable to verify new config was sucessfully downloaded"
             throw
         }
